8c00af09d311f18195208ce9d6a7613cb7316415
Kushagra Gupta <kushagragpt7@gmail.com>
2023-10-31 13:02:29 -0500

    LQAC added, static solver works now

--- src/TwoPlayer.jl
+++ src/TwoPlayer.jl
@@ -11,8 +11,11 @@ function solve_zero_sum(j1, x, tol, total_iter, α=1.0)
         D[1,2] = -400*x[1]
         D[2,1] = 400*x[1]
         D[2,2] = -200
-        mat_Reg = circle_theorem_regularize(D'*D)
-        new_x = x - α*inv(mat_Reg)*D'*F
+        hess_p1_reg = circle_theorem_regularize([D[1,1]])
+        hess_p2_reg = circle_theorem_regularize([-1.0*D[2,2]])
+        D[1,1] = hess_p1_reg[1,1]
+        D[2, 2] = -1.0*hess_p2_reg[1,1]
+        new_x = x - α*inv(D)*F
         error = norm(new_x - x)
         x = new_x
         k += 1
@@ -23,7 +26,7 @@ function solve_zero_sum(j1, x, tol, total_iter, α=1.0)
     return x, j1(x), -j1(x), k
 end
 
-Point, J_1, J_2, iters = solve_zero_sum(rosenbrock, [0.9; 0.9], 10e-9, 100000, 0.5)
+Point, J_1, J_2, iters = solve_zero_sum(rosenbrock, [20, -30], 10e-5, 100000, 0.5)
 println("P1_Strat: ", Point[1])
 println("P2_Strat: ", Point[2])
 println("J1: ", J_1)
