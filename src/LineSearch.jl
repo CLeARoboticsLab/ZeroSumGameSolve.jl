93ac152749b76f0c9196bbee3566d111242e1e20
xinjie-liu <xinjieliu98@163.com>
2024-05-12 12:06:36 -0500

    line search

--- src/LineSearch.jl
+++ src/LineSearch.jl
@@ -1,11 +1,25 @@
 using ZeroSumGameSolve
 using LinearAlgebra
+using ReverseDiff
+using CUDA
+using Zygote: Zygote
+
+
+
+function norm_w(x, func)
+    grads = Zygote.gradient(x) do x
+        func(x)
+    end
+    return 0.5*norm(grads[1])^2
+end
 
 export alpha
-function alpha(point, func, n_x)
-    hess = zero_sum_hessian(point, func, n_x)
-    hess2 = hess'
-    mat = hess + hess2
-    eigenvalues = eigvals(mat)
-    return minimum([1.98*abs(minimum(eigenvalues)), 0.00001])
+function alpha(point, func, step, w, J, c)
+    m = transpose(w)*J*step
+    α = 1.0
+    while norm_w(point, func) - norm_w(point - α*step, func) < c*α*m
+        α = 0.5*α
+    end
+
+    return α
 end
