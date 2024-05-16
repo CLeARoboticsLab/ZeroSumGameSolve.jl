cc4d13e295ab80cde39a03903f1522c38dce42e8
xinjie-liu <xinjieliu98@163.com>
2024-05-16 17:39:55 -0500

    fix bug in line search

--- src/LineSearch.jl
+++ src/LineSearch.jl
@@ -24,6 +24,7 @@ function alpha(point, func, step, w, J, c=1e-4)
     point_new = point - α*update
     while (norm_w(point, func) - norm_w(point_new, func) < c*α*m) && α >= 1e-8
         α = 0.5*α
+        point_new = point - α*update
     end
 
     return α
