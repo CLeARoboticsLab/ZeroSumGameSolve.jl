0a8d1207136e15932b8f1cde85b669e93a5980ac
xinjie-liu <xinjieliu98@163.com>
2024-05-16 22:27:32 -0500

    changing activation function to tanh

--- experiments/GANExample/src/gan.jl
+++ experiments/GANExample/src/gan.jl
@@ -188,13 +188,13 @@ end
 
 function setup_gan(set_up = construct_training_setup(); generator = nothing, discriminator = nothing)
     discriminator = isnothing(discriminator) ? Chain(
-        Dense(set_up.dims.dim_x, set_up.dims.dim_hidden, relu; init = glorot_uniform(set_up.rng)),
-        Dense(set_up.dims.dim_hidden, set_up.dims.dim_hidden, relu; init = glorot_uniform(set_up.rng)),
+        Dense(set_up.dims.dim_x, set_up.dims.dim_hidden, tanh; init = glorot_uniform(set_up.rng)),
+        Dense(set_up.dims.dim_hidden, set_up.dims.dim_hidden, tanh; init = glorot_uniform(set_up.rng)),
         Dense(set_up.dims.dim_hidden, 1, sigmoid; init = glorot_uniform(set_up.rng)),
     ) : discriminator
     generator = isnothing(generator) ? Chain(
-        Dense(set_up.dims.dim_z, set_up.dims.dim_hidden, relu; init = glorot_uniform(set_up.rng)),
-        Dense(set_up.dims.dim_hidden, set_up.dims.dim_hidden, relu; init = glorot_uniform(set_up.rng)),
+        Dense(set_up.dims.dim_z, set_up.dims.dim_hidden, tanh; init = glorot_uniform(set_up.rng)),
+        Dense(set_up.dims.dim_hidden, set_up.dims.dim_hidden, tanh; init = glorot_uniform(set_up.rng)),
         Dense(set_up.dims.dim_hidden, set_up.dims.dim_x; init = glorot_uniform(set_up.rng)),
     ) : generator
     GAN(set_up.dims.dim_z, generator, discriminator) |> set_up.training_config.device
