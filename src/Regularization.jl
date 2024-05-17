9c380b965f6b630c23b25899a1c0d154559a92b0
xinjie-liu <xinjieliu98@163.com>
2024-05-16 20:44:38 -0500

    minor bug fix

--- src/Regularization.jl
+++ src/Regularization.jl
@@ -4,7 +4,7 @@ using ZeroSumGameSolve
 export regularization
 function regularization(x, update_step_grad, update_step_hess, tol=1e-8)
     if norm(update_step_grad) < tol
-        return zeros(size(update_step_grad))
+        return [@SVector [0.0] for i in 1:size(update_step_grad)[1]]
     else
         mat = update_step_hess + transpose(update_step_hess)
         return inv(circle_theorem_regularize(mat*mat*mat))*transpose(update_step_hess)*update_step_grad
